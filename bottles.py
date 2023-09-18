fe6c15fcad38e74f4c7c897f69069f4b52d9f059
Zijie Wang <zwang2892@wisc.edu>
2023-09-18 14:12:40 -0500

    changed the lyrics

--- bottles.py
+++ bottles.py
@@ -1,4 +1,4 @@
-beverage = "water"
+beverage = "Cola"
 count = 2
 
 print(count, "bottles of", beverage, "on the wall")
@@ -12,5 +12,5 @@ print("")
 print(count, "bottles of", beverage, "on the wall")
 print(count, "bottles of", beverage)
 print("Take one down, pass it around")
-count = count - 1
+count = count + 1
 print(count, "bottles of", beverage, "on the wall")
