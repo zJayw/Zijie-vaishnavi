0b711f1ccfe9724674803121ccce228c48ae4032
Zijie Wang <zwang2892@wisc.edu>
2023-09-18 22:00:14 -0500

    finished the Ex5 and Ex6

--- bottles.py
+++ bottles.py
@@ -1,10 +1,10 @@
 beverage = "Cola"
-count = 2
+count = 5
 
 print(count, "bottles of", beverage, "on the wall")
 print(count, "bottles of", "water")
 print("Take one down, pass it around")
-count = count - 1
+count = count - 3
 print(count, "bottles of", beverage, "on the wall")
 
 print("")
@@ -12,5 +12,5 @@ print("")
 print(count, "bottles of", beverage, "on the wall")
 print(count, "bottles of", beverage)
 print("Take one down, pass it around")
-count = count + 1
+count = count - 2
 print(count, "bottles of", beverage, "on the wall")
